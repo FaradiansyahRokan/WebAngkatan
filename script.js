de9ea778409ab3f82df7c73f22659101d6626b38
rokan <rokansubhi.f@gmail.com>
2023-08-22 07:51:39 +0700

    Almost

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+var flag = 0;
+var menu = document.querySelector('#menu');
+var line1 = document.getElementById('line1');
+var line2 = document.getElementById('line2');
+menu.addEventListener('click', () => {
+  if (flag === 0) {
+    document.getElementById('topmenu').style.transform = `translateY(0)`
+    line1.style.backgroundColor = `#232025`;
+    line2.style.backgroundColor = `#232025`;
+    line1.style.transform = `rotate(40deg)`;
+    line1.style.width = `70%`;
+    line2.style.transform = `rotate(-40deg)`;
+    line2.style.backgroundColor = `#232025`;
+    document.getElementById('nav').style.color = `#232025`;
+    flag = 1;
+  }
+  else {
+    document.getElementById('topmenu').style.transform = `translateY(-100%)`
+    line1.style.backgroundColor = `#cecece`;
+    line2.style.backgroundColor = `#cecece`;
+    line1.style.transform = `rotate(00deg)`;
+    line1.style.width = `100%`;
+    line2.style.transform = `rotate(-0deg)`;
+    line2.style.backgroundColor = `#cecece`;
+    document.getElementById('nav').style.color = `#cecece`;
+    flag = 0;
+  }
+})
+
 function init() {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -145,6 +174,17 @@ gsap.to('#finish', {
         $('#finish h1').textillate({ in: { effect: 'fadeInUp' } });
       })
     },
+  })
+  gsap.to('.page4 #line', {
+    scrollTrigger: {
+      trigger: '.page4',
+      scroller: '.main',
+      scrub: 2,
+      end: 'bottom 90%',
+      // markers:true,
+      start: 'top 80%',
+    },
+    width: '100%',
   })
   
   var slide1h1 = document.querySelectorAll('#page8 .slide1h1 h1');
